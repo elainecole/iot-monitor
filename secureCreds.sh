fcf83fef8c9798f7b98762a5f7b01869dee7197d
Sum B0dy <f.tavares@wustl.edu>
2019-12-04 13:52:02 -0600

    scripts added

---
+++ secureCreds.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+if [ $# -ne 1 ] ; then
+    echo "USAGE: $0 <vulnerable ip list>"
+    exit 1
+fi
+
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+command -v expect >/dev/null 2>&1 || { 
+    echo "*********";
+    echo -e >&2 "${RED} expect not installed.${NC}";
+    echo "*********";
+    echo "sudo apt-get install expect" ;
+    echo "--------------------"
+    exit 1; 
+}
+
+
+
+file=$1 
+IFS=$'\n' read -d '' -r -a lines < $1
+# printf "line 1: %s\n" "${lines[1]}"
+
+own_ip=$(hostname -I)
+
+for i in "${lines[@]}"
+do
+
+    # echo "Vunerable Machine : $i"
+    # echo "Would you like to change the default password of vulnerable devices? "
+    username=$(echo $i | cut -d " " -f2)
+    old_pass=$(echo $i | cut -d " " -f3)
+    hostip=$(echo $i | cut -d " " -f1)
+
+    echo $hostip $own_ip
+    if [ $hostip = $own_ip ] ; then
+        echo "my ip is in the list"
+        exit 1
+    else
+        echo "Vunerable Machine : $i"
+        echo "Would you like to change the default password of vulnerable devices? "
+        while true; do 
+            read -p "Y/n :" ans
+            echo $ans
+                case $ans in
+                    'Y') 
+                        echo "What would you like the new password to be?";
+                        read -sp 'Password: ' passvar;
+                        echo " ";
+                        ./ssh_change $username $old_pass $passvar $hostip >/dev/null
+                        break ;;
+                    'n') 
+                        echo ".";
+                        echo ".";
+                        echo ".";
+                        sleep 1;
+                        break ;;
+                    *) echo "Invalid Response";;
+                esac
+        done
+    fi
+
+    # while true; do 
+    # read -p "Y/n :" ans
+    # echo $ans
+    #     case $ans in
+    #         'Y') 
+    #             echo "What would you like the new password to be?";
+    #             # read -p 'Username: ' uservar
+    #             read -sp 'Password: ' passvar;
+    #             echo " ";
+    #             ./ssh_change $username $old_pass $passvar $hostip
+    #             break ;;
+    #         'n') 
+    #             echo ".";
+    #             echo ".";
+    #             echo ".";
+    #             sleep 1;
+    #             break ;;
+    #         *) echo "Invalid Response";;
+    #     esac
+    # done
+
+
+done
+
---
+++ whiteWorm.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# colors usage : ${RED}Possible base 64 detected :${NC}
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+command -v nmap >/dev/null 2>&1 || { 
+    echo "*********";
+    echo -e >&2 "${RED} nmap not installed.${NC}";
+    echo "*********";
+    echo "sudo apt install nmap" ;
+    echo "--------------------"
+    exit 1; 
+}
+
+
+
+# rm tmp_ip.txt 2>/dev/null
+rm ip_vulnerable.txt 2>/dev/null
+echo "-----Scanning for devices in the network-----"
+getip=$(hostname -I | cut -d "." -f1-3)
+
+#initial identification of machines in the network
+nmap -n -sn ${getip}.1/24
+
+# touch tmp_ip.txt
+arp -a -n  | grep -v incomplete | awk '{print $2}' | tr -d '()' > tmp
+
+max_ip=$(sort -r tmp | grep -m1 "" | cut -d '.' -f4)
+
+# echo $max_ip
+
+echo "-----checking for open port 22-------"
+
+echo " " >/dev/null
+#echo "---PORT 22 ---" >> ip_vulnerable.txt
+result22=$(nmap -PN -p 22 --open -oG - ${getip}.0-$max_ip | awk '$NF~/ssh/{print $2}' )
+echo "$result22" >> ip_vulnerable.txt
+echo -e "${GREEN}done ${NC}"
+
+echo "-----checking for open port 23-------"
+
+# echo "---PORT 23 ---" >> ip_vulnerable.txt
+result23=$(nmap -PN -p 23 --open -oG - ${getip}.0-$max_ip | awk '$NF~/telnet/{print $2}')
+# echo "$result23" >> ip_vulnerable.txt
+echo -e "${GREEN}done ${NC}"
+
+echo "-----Showing results-------"
+cat ip_vulnerable.txt
+
